665c6b721d18daf88b21d88125debc6f6b210894
BladeLordLyndis <shu0018x@outlook.com>
2016-05-30 00:56:36 -0400

    Added default constructor for Array.

--- Collection/src/Collection/Array.java
+++ Collection/src/Collection/Array.java
@@ -1,5 +1,15 @@
 package Collection;
 
 public class Array {
-
+  private static int DEFAULT_CAPACITY = 10;
+  
+  private int size;
+  private int capacity;
+  private int[] array;
+  
+  public Array() {
+    capacity = DEFAULT_CAPACITY;
+    size = 0;
+    array = new int[DEFAULT_CAPACITY];
+  }
 }
